bbc5c44c7b8cfe5de7af9f1f6a94b1cd965e64e0
robin4250 <robin4250@gmail.com>
2026-09-17 13:05:12 +0900

    Add settings screen widget test

---
+++ test/settings_page_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sk_works/features/settings/settings_page.dart';
+
+void main() {
+  testWidgets('settings page shows company and invoice defaults', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+    await tester.pumpWidget(const MaterialApp(home: SettingsPage()));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('会社情報'), findsOneWidget);
+    expect(find.text('請求設定'), findsOneWidget);
+    expect(find.text('設定を保存'), findsOneWidget);
+    expect(find.widgetWithText(TextField, 'SK WORKS'), findsOneWidget);
+  });
+}
