5abde6e875c28cefd2375d2465eca1c567107494
robin4250 <robin4250@gmail.com>
2026-09-17 12:45:28 +0900

    Stabilize widget test persistence setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sk_works/main.dart';
 
 void main() {
   testWidgets('SK WORKS home and employee module work', (tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const SkWorksApp());
 
     expect(find.text('SK WORKS'), findsWidgets);
@@ -11,7 +14,8 @@ void main() {
     expect(find.text('現場管理'), findsOneWidget);
 
     await tester.tap(find.text('社員・協力会社'));
-    await tester.pumpAndSettle();
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
 
     expect(find.text('山田 太郎'), findsOneWidget);
     expect(find.text('株式会社サンプル工業'), findsOneWidget);
