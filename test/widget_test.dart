ed1220d979e8661380a6ab07a17a15ad14f8464f
robin4250 <robin4250@gmail.com>
2026-09-17 12:34:32 +0900

    Expand widget test for core navigation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,14 +2,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sk_works/main.dart';
 
 void main() {
-  testWidgets('SK WORKS home screen is shown', (tester) async {
+  testWidgets('SK WORKS home and employee module work', (tester) async {
     await tester.pumpWidget(const SkWorksApp());
 
     expect(find.text('SK WORKS'), findsWidgets);
     expect(find.text('社員・協力会社'), findsOneWidget);
     expect(find.text('資格管理'), findsOneWidget);
     expect(find.text('現場管理'), findsOneWidget);
-    expect(find.text('勤怠・人工'), findsOneWidget);
-    expect(find.text('請求管理'), findsOneWidget);
+
+    await tester.tap(find.text('社員・協力会社'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('山田 太郎'), findsOneWidget);
+    expect(find.text('株式会社サンプル工業'), findsOneWidget);
+    expect(find.text('新規登録'), findsOneWidget);
   });
 }
