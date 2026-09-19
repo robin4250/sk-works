4af67d95813ae743128b7414e36dbd53ff68f639
robin4250 <robin4250@gmail.com>
2026-09-19 20:53:42 +0900

    Update home widget test for finalized admin dashboard

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,22 +3,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sk_works/main.dart';
 
 void main() {
-  testWidgets('SKO home and employee module work', (tester) async {
+  testWidgets('SKO admin home uses finalized dashboard labels', (tester) async {
     SharedPreferences.setMockInitialValues({});
 
     await tester.pumpWidget(const SkWorksApp());
+    await tester.pump();
 
     expect(find.text('SKO'), findsWidgets);
-    expect(find.text('社員・協力会社'), findsOneWidget);
-    expect(find.text('資格管理'), findsOneWidget);
-    expect(find.text('現場管理'), findsOneWidget);
-
-    await tester.tap(find.text('社員・協力会社'));
-    await tester.pump();
-    await tester.pump(const Duration(milliseconds: 100));
-
-    expect(find.text('山田 太郎'), findsOneWidget);
-    expect(find.text('株式会社サンプル工業'), findsOneWidget);
-    expect(find.text('新規登録'), findsOneWidget);
+    expect(find.text('本日の出勤'), findsOneWidget);
+    expect(find.text('承認待ち'), findsOneWidget);
+    expect(find.text('出勤・人区管理'), findsOneWidget);
+    expect(find.text('人員管理'), findsOneWidget);
+    expect(find.text('請求書'), findsOneWidget);
+    expect(find.text('管理者用現場データ'), findsOneWidget);
   });
 }
