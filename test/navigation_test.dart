19d8cb4951a275421d3a8f0dd862a16a937cc939
robin4250 <robin4250@gmail.com>
2026-09-19 22:00:25 +0900

    Scroll lazy menu list to secondary functions in test

--- test/navigation_test.dart
+++ test/navigation_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sk_works/main.dart';
@@ -64,8 +65,11 @@ void main() {
     expect(find.text('日報'), findsOneWidget);
     expect(find.text('プロフィール'), findsOneWidget);
     expect(find.text('ヘルプ'), findsOneWidget);
-    await tester.ensureVisible(find.text('資格'));
-    await tester.pump();
+    await tester.scrollUntilVisible(
+      find.text('資格'),
+      200,
+      scrollable: find.byType(Scrollable).last,
+    );
     expect(find.text('資格'), findsOneWidget);
   });
 }
