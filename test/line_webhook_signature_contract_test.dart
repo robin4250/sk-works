3a5203fadf2ccf51752cbdc22739edfa89e3eb2e
robin4250 <robin4250@gmail.com>
2026-09-21 06:47:06 +0900

    Assert webhook signature precedes database access

--- test/line_webhook_signature_contract_test.dart
+++ test/line_webhook_signature_contract_test.dart
@@ -13,5 +13,10 @@ void main() {
     expect(source, contains('LINE_CHANNEL_SECRET'));
     expect(source, contains('Invalid signature'));
     expect(source, contains('status: 401'));
+
+    final signatureCheck = source.indexOf('receivedSignature !== expectedSignature');
+    final firstDatabaseCall = source.indexOf('rest("line_webhook_events');
+    expect(signatureCheck, greaterThanOrEqualTo(0));
+    expect(firstDatabaseCall, greaterThan(signatureCheck));
   });
 }
