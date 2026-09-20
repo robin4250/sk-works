3259a73b3afd6bd0555e24ac2f695f3d8136e8b5
robin4250 <robin4250@gmail.com>
2026-09-21 04:18:35 +0900

    Import Material widgets in layout test

--- test/iphone_layout_readiness_test.dart
+++ test/iphone_layout_readiness_test.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sk_works/features/home/friendly_home_content.dart';
