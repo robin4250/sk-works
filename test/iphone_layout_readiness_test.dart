9c25d3beefdb0507911fd65832a8ec646e366c7b
robin4250 <robin4250@gmail.com>
2026-09-21 04:16:13 +0900

    Test long company and user names on iPhone

--- test/iphone_layout_readiness_test.dart
+++ test/iphone_layout_readiness_test.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sk_works/features/home/friendly_home_content.dart';
+import 'package:sk_works/features/home/home_membership_repository.dart';
 import 'package:sk_works/main.dart';
 
 void main() {
@@ -50,6 +52,41 @@ void main() {
 
     expect(find.text('プロフィール'), findsOneWidget);
     expect(find.text('ヘルプ'), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+
+  testWidgets('long company and user names fit iPhone portrait', (tester) async {
+    tester.view.physicalSize = const Size(375, 812);
+    tester.view.devicePixelRatio = 1;
+    tester.platformDispatcher.textScaleFactorTestValue = 1.4;
+
+    addTearDown(() {
+      tester.view.resetPhysicalSize();
+      tester.view.resetDevicePixelRatio();
+      tester.platformDispatcher.clearTextScaleFactorTestValue();
+    });
+
+    const identity = HomeIdentity(
+      role: 'viewer',
+      companyName: '株式会社エスケーワークス東京都墨田区総合建設事業部とても長い会社名',
+      displayName: '齊藤竜一とても長い表示名テスト',
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: FriendlyHomeContent(
+            identity: identity,
+            moduleEnabled: (_) => true,
+            onOpen: (_) async {},
+            onRefresh: () async {},
+          ),
+        ),
+      ),
+    );
+    await tester.pump();
+
+    expect(find.text(identity.companyName), findsOneWidget);
     expect(tester.takeException(), isNull);
   });
 
