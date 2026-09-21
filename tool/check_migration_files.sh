9e753ef93d24178ae5095c6c745f33fe08bc943c
robin4250 <robin4250@gmail.com>
2026-09-22 02:29:48 +0900

    Require approval permission alignment migration

--- tool/check_migration_files.sh
+++ tool/check_migration_files.sh
@@ -43,6 +43,7 @@ required=(
   "20260922004500_add_employee_onboarding_profile_and_approval.sql"
   "20260922010000_add_required_document_attention.sql"
   "20260922013000_add_admin_initial_setup_wizard.sql"
+  "20260922021500_align_approval_assignee_permission_source.sql"
 )
 
 for name in "${required[@]}"; do
