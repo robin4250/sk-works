0aa77fb53a05b6d40f99e566b346636c76eb27bd
robin4250 <robin4250@gmail.com>
2026-09-22 03:33:37 +0900

    Require invite role and approver migration

--- tool/check_migration_files.sh
+++ tool/check_migration_files.sh
@@ -44,6 +44,7 @@ required=(
   "20260922010000_add_required_document_attention.sql"
   "20260922013000_add_admin_initial_setup_wizard.sql"
   "20260922021500_align_approval_assignee_permission_source.sql"
+  "20260922024000_add_invite_role_and_approver_selection.sql"
 )
 
 for name in "${required[@]}"; do
