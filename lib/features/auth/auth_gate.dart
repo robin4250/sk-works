02e20202237ae3676c77d65be6c7675e61c65b71
robin4250 <robin4250@gmail.com>
2026-09-22 01:12:50 +0900

    Rebase admin initial setup onto latest main

--- lib/features/auth/auth_gate.dart
+++ lib/features/auth/auth_gate.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../data/supabase_backend.dart';
-import 'employee_onboarding_pages.dart';
+import 'admin_initial_setup_page.dart';
+import 'admin_initial_setup_repository.dart';
 import 'secure_onboarding_pages.dart';
 import 'secure_onboarding_repository.dart';
 
@@ -37,29 +38,20 @@ class _SupabaseAuthGateState extends State<SupabaseAuthGate> {
       return const _GateState.unauthenticated();
     }
 
-    final employee = await repository.employeeOnboardingState();
-    if (employee != null && employee.status == 'cancelled') {
-      return _GateState.employeeInviteInvalid(employee);
-    }
-
-    if (employee != null && employee.status != 'approved') {
-      if (employee.needsPrimaryPassword) {
-        return _GateState.employeePassword(employee);
-      }
-      if (employee.needsProfile) {
-        return _GateState.employeeProfile(employee);
-      }
-      if (employee.awaitingApproval) {
-        return _GateState.employeeApprovalPending(employee);
-      }
-    }
-
     final hasCompany = await repository.hasCompanyMembership();
     if (!hasCompany) {
       if (repository.companySetupDeferred) {
         return const _GateState.companyDeferred();
       }
       return const _GateState.needsCompany();
+    }
+
+    final adminSetupRepository = AdminInitialSetupRepository.maybeCreate();
+    if (adminSetupRepository != null) {
+      final setup = await adminSetupRepository.loadState();
+      if (setup.required && !setup.completed) {
+        return const _GateState.needsAdminInitialSetup();
+      }
     }
 
     return const _GateState.authenticated();
@@ -102,24 +94,6 @@ class _SupabaseAuthGateState extends State<SupabaseAuthGate> {
         return switch (state.status) {
           _GateStatus.unauthenticated =>
             SecureAuthPage(onAuthenticated: _reload),
-          _GateStatus.employeePassword => EmployeePrimaryPasswordPage(
-              name: state.employee!.name,
-              onCompleted: _reload,
-              onSignOut: _signOut,
-            ),
-          _GateStatus.employeeProfile => EmployeeProfileOnboardingPage(
-              name: state.employee!.name,
-              onSubmitted: _reload,
-              onSignOut: _signOut,
-            ),
-          _GateStatus.employeeApprovalPending => EmployeeApprovalWaitingPage(
-              name: state.employee!.name,
-              onRefresh: _reload,
-              onSignOut: _signOut,
-            ),
-          _GateStatus.employeeInviteInvalid => EmployeeInviteInvalidPage(
-              onSignOut: _signOut,
-            ),
           _GateStatus.needsCompany => CompanyProfileSetupPage(
               onCompleted: _reload,
               onDeferred: _reload,
@@ -127,6 +101,10 @@ class _SupabaseAuthGateState extends State<SupabaseAuthGate> {
             ),
           _GateStatus.companyDeferred => DeferredCompanySetupPage(
               onResume: _resumeCompanySetup,
+              onSignOut: _signOut,
+            ),
+          _GateStatus.needsAdminInitialSetup => AdminInitialSetupWizardPage(
+              onCompleted: _reload,
               onSignOut: _signOut,
             ),
           _GateStatus.authenticated => widget.homeBuilder(_signOut),
@@ -189,35 +167,25 @@ class _ErrorScreen extends StatelessWidget {
 
 enum _GateStatus {
   unauthenticated,
-  employeePassword,
-  employeeProfile,
-  employeeApprovalPending,
-  employeeInviteInvalid,
   needsCompany,
   companyDeferred,
+  needsAdminInitialSetup,
   authenticated,
 }
 
 class _GateState {
-  const _GateState._(this.status, [this.employee]);
+  const _GateState._(this.status);
 
   const _GateState.unauthenticated()
       : this._(_GateStatus.unauthenticated);
-  const _GateState.employeePassword(EmployeeOnboardingState employee)
-      : this._(_GateStatus.employeePassword, employee);
-  const _GateState.employeeProfile(EmployeeOnboardingState employee)
-      : this._(_GateStatus.employeeProfile, employee);
-  const _GateState.employeeApprovalPending(EmployeeOnboardingState employee)
-      : this._(_GateStatus.employeeApprovalPending, employee);
-  const _GateState.employeeInviteInvalid(EmployeeOnboardingState employee)
-      : this._(_GateStatus.employeeInviteInvalid, employee);
   const _GateState.needsCompany()
       : this._(_GateStatus.needsCompany);
   const _GateState.companyDeferred()
       : this._(_GateStatus.companyDeferred);
+  const _GateState.needsAdminInitialSetup()
+      : this._(_GateStatus.needsAdminInitialSetup);
   const _GateState.authenticated()
       : this._(_GateStatus.authenticated);
 
   final _GateStatus status;
-  final EmployeeOnboardingState? employee;
 }
