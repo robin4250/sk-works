2d73e95e5d9436b7cb979246fb566d6e4e049ca2
robin4250 <robin4250@gmail.com>
2026-09-22 00:38:32 +0900

    Update iPhone home layout for document attention

--- test/iphone_layout_readiness_test.dart
+++ test/iphone_layout_readiness_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sk_works/features/home/friendly_home_content.dart';
+import 'package:sk_works/features/home/home_attention_repository.dart';
 import 'package:sk_works/features/home/home_membership_repository.dart';
 import 'package:sk_works/main.dart';
 
@@ -76,6 +77,12 @@ void main() {
         home: Scaffold(
           body: FriendlyHomeContent(
             identity: identity,
+            requiredDocumentAttention: const RequiredDocumentAttention(
+              missingCount: 0,
+              missingNames: [],
+              needsLicense: false,
+              needsQualification: false,
+            ),
             moduleEnabled: (_) => true,
             onOpen: (_) async {},
             onRefresh: () async {},
