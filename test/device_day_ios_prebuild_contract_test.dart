cc53767067fdda81762c78406abbd41f5f410117
robin4250 <robin4250@gmail.com>
2026-09-21 12:55:37 +0900

    Test device-day unsigned iOS prebuild

---
+++ test/device_day_ios_prebuild_contract_test.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('device-day preflight builds iOS before signed device launch', () {
+    final preflight =
+        File('tool/device_day_preflight.sh').readAsStringSync();
+    final diagnostics =
+        File('tool/collect_ios_diagnostics.sh').readAsStringSync();
+
+    final buildIndex =
+        preflight.indexOf('flutter build ios --debug --no-codesign');
+    final assistantIndex =
+        preflight.indexOf('bash tool/ios_install_assistant.sh');
+
+    expect(buildIndex, greaterThanOrEqualTo(0));
+    expect(assistantIndex, greaterThan(buildIndex));
+    expect(preflight, contains('Podfile.lock'));
+    expect(diagnostics, contains('ios/Podfile.lock'));
+  });
+}
