321ab805de8ca9dc2362a707d8cb658ac3192698
robin4250 <robin4250@gmail.com>
2026-09-22 01:41:57 +0900

    Rebase admin initial setup onto latest main

--- lib/features/auth/auth_gate.dart
+++ lib/features/auth/auth_gate.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../data/supabase_backend.dart';
+import 'admin_initial_setup_page.dart';
+import 'admin_initial_setup_repository.dart';
 import 'employee_onboarding_pages.dart';
 import 'secure_onboarding_pages.dart';
 import 'secure_onboarding_repository.dart';
@@ -60,6 +62,14 @@ class _SupabaseAuthGateState extends State<SupabaseAuthGate> {
         return const _GateState.companyDeferred();
       }
       return const _GateState.needsCompany();
+    }
+
+    final adminSetupRepository = AdminInitialSetupRepository.maybeCreate();
+    if (adminSetupRepository != null) {
+      final setup = await adminSetupRepository.loadState();
+      if (setup.required && !setup.completed) {
+        return const _GateState.needsAdminInitialSetup();
+      }
     }
 
     return const _GateState.authenticated();
@@ -129,6 +139,10 @@ class _SupabaseAuthGateState extends State<SupabaseAuthGate> {
               onResume: _resumeCompanySetup,
               onSignOut: _signOut,
             ),
+          _GateStatus.needsAdminInitialSetup => AdminInitialSetupWizardPage(
+              onCompleted: _reload,
+              onSignOut: _signOut,
+            ),
           _GateStatus.authenticated => widget.homeBuilder(_signOut),
         };
       },
@@ -195,6 +209,7 @@ enum _GateStatus {
   employeeInviteInvalid,
   needsCompany,
   companyDeferred,
+  needsAdminInitialSetup,
   authenticated,
 }
 
@@ -215,6 +230,8 @@ class _GateState {
       : this._(_GateStatus.needsCompany);
   const _GateState.companyDeferred()
       : this._(_GateStatus.companyDeferred);
+  const _GateState.needsAdminInitialSetup()
+      : this._(_GateStatus.needsAdminInitialSetup);
   const _GateState.authenticated()
       : this._(_GateStatus.authenticated);
 
