47cf58b73733dbe5620ce91015f42938481bfd27
robin4250 <robin4250@gmail.com>
2026-09-24 03:26:41 +0900

    Load saved SKO color palette on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'app_v2.dart';
+import 'branding/sko_theme.dart';
 import 'data/supabase_backend.dart';
 
 export 'app_v2.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await SkoThemeController.load();
+  } catch (_) {
+    // Keep the default SKO palette if local preferences cannot be read.
+  }
   try {
     await SupabaseBackend.initializeIfConfigured();
   } catch (_) {
